ee4a2d48fef776708289c970acd05324432af5e0
James Lee <lee963654@gmail.com>
2023-04-21 14:17:10 -0400

    update finished all, now will test and refactor in the dev branch

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -27,9 +27,11 @@ const validateCreateSpot = [
         .withMessage('Country is required'),
     check('lat')
         .exists({ checkFalsy: true})
+        .isFloat({ min: -90, max: 90})
         .withMessage('Latitude is not valid'),
     check('lng')
         .exists({ checkFalsy: true})
+        .isFloat({ min: -180, max: 180})
         .withMessage('Longitude is not valid'),
     check('name')
         .exists({ checkFalsy: true})
