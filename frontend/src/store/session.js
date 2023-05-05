0c2a0488f3f8da8b1cb3e71a3b7ed63abdeecf27
James Lee <lee963654@gmail.com>
2023-05-04 23:12:09 -0400

    finished phase1

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+// ACTIONS
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +17,9 @@ const removeUser = () => {
   };
 };
 
+
+
+// THUNK
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -30,6 +34,16 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+
+
 
 const initialState = { user: null };
 
