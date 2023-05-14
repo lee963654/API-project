5ccea12c64fcb95eb54e2a2c21fa9613e3642ecb
James Lee <lee963654@gmail.com>
2023-05-14 09:59:54 -0400

    working on user delete and update

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
+
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
+  const history = useHistory()
 
   return (
     <div className="header-container">
       <div className="header">
-        <NavLink exact to="/" className="header-links logo">!AirBnB</NavLink>
+        <NavLink exact to="/" className="header-links logo"><img src="https://files.prokerala.com/news/photos/imgs/1024/airbnb-logo-653256.jpg" style={{width: 35, height: 35}}></img></NavLink>
+        <div onClick={() => history.push("/")} className="airbnb-logo">!AirBnB</div>
       </div>
       <div className="create-and-profile">
         {sessionUser && (
